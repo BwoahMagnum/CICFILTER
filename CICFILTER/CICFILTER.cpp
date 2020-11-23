0968524645d3c72992efef7043e6e13e7848a6be
Cristiano <liyfcristiano@gmail.com>
2020-11-24 00:03:30 +0800

    从C到C++，用类真累...

--- CICFILTER/CICFILTER.cpp
+++ CICFILTER/CICFILTER.cpp
@@ -6,3 +6,26 @@
 //
 
 #include "CICFILTER.hpp"
+class CIC_FILTER{
+public:
+    CIC_FILTER(bool U, int W=1, int R=1, int N=1, int M=1);
+    ~CIC_FILTER();
+    // void INIT();
+
+private:
+    int     ratio;
+    int     stage;
+    int     sample;
+    int     channel;
+    bool    util;
+    double* Z_Stage_Comb;
+    double* Z_Stage_Integrator;
+};
+
+CIC_FILTER::CIC_FILTER(bool U, int W, int R, int N, int M){
+    util    = U;
+    ratio   = R;
+    stage   = N;
+    sample  = M;
+    channel = W;
+}
--- CICFILTER/CICFILTER.hpp
+++ CICFILTER/CICFILTER.hpp
@@ -7,6 +7,8 @@
 
 #ifndef CICFILTER_hpp
 #define CICFILTER_hpp
+#define CIC_Interpolation   false
+#define CIC_Decimation      true
 
 #include <stdio.h>
 
