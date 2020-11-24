669601b0c91301d74a8b61636c2e24f1cbe875fc
Cristiano <liyfcristiano@gmail.com>
2020-11-25 00:34:31 +0800

    out of date?wtf

--- CICFILTER/CICFILTER.cpp
+++ CICFILTER/CICFILTER.cpp
@@ -17,19 +17,26 @@ CIC_FILTER::CIC_FILTER(bool U, int W, int R, int N, int M){
     channel = (W>0)?W:1;
     
     Z_Stage_Comb        = new double[channel*stage*sample];
-    Z_Stage_Integrator  = new double[channel*stage*sample];
+    Z_Stage_Integrator  = new double[channel*stage];
     
     cout<<"sample actually is "<<sample<<endl;
     for (int i=0; i<stage; i++)
         cout<<Z_Stage_Comb[i]<<endl;
     cout<<"Class CIC_FILTER has been created successfully!"<<endl;
 }
-void CIC_FILTER::SetParam(bool U, int W, int R, int N, int M)
-{
-}
-double* CIC_FILTER::Output(double* data)
-{
+void CIC_FILTER::SetParam(bool U, int W, int R, int N, int M){
     
+}
+double* CIC_FILTER::Output(double* data){
+    double temp;
+    for (int i; i<channel; i++){
+        // comb
+        temp = data[i];
+        for(int j; i< stage; j++){
+            temp -= Z_Stage_Comb[i*j*sample];
+        }
+
+    }
     return result;
 }
 CIC_FILTER::~CIC_FILTER(void){
