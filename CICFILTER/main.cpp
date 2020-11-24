885e3215fc1011547cdb600f4bae4e5055c4bc3a
Cristiano <liyfcristiano@gmail.com>
2020-11-25 00:05:26 +0800

    routine

--- CICFILTER/main.cpp
+++ CICFILTER/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     double* y;
+    y=NULL;
     CIC_FILTER x(CIC_Interpolation, 1, 1, 3, 3);
     cout << "Hello, World!\n";
     // CIC_FILTER(x, 1);
